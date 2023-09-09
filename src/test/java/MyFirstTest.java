94e51b1a038cf7256054204f8c5aee1ca6c4d00c
Abul717 <steve003001@gmail.com>
2023-09-09 11:23:08 +0400

    Firefox

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -1,22 +1,16 @@
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class MyFirstTest {
     @Test
-    public void myFirstTestCase() throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        ChromeDriver driver = new ChromeDriver();
+    public void FirefoxTestCase() throws InterruptedException {
+        System.setProperty("webdriver.gecko.driver", "src/test/resources/geckodriver.exe");
+        FirefoxDriver driver = new FirefoxDriver();
         driver.get("https://www.facebook.com/");
         Thread.sleep(5000);
         driver.quit();
     }
-    @Test
-    public void mySecondTestCase() throws IneterruptedException {
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        ChromeDriver driver = new ChromeDriver();
-        driver.get("https://www.google.com/");
-        Thread.sleep(5000);
-        driver.quit();
-    }
+
 
 }
